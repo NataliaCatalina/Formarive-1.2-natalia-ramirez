74aa128fcfe5a3afffb6a09caaa26c806ee52175
Natalia <ntlc.ramirez@gmail.com>
2020-08-26 15:31:14 +1200

    final

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 console.log('js');
-
+// $('.top__navegation--menu').hide();
 // $('.blue-block').hide();
 // $('.socials').hide();
 
 $(document).ready(function(){
 
   $('.menu-icon').click(function(){
+    console.log('clicked');
     $('.top__navegation--menu').toggleClass('active');
   });
 
