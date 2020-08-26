b55529cda5f1698801c0c8a3f784b7cf652f7ca3
Natalia <ntlc.ramirez@gmail.com>
2020-08-26 12:54:57 +1200

    final

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 console.log('js');
 
+// $('.blue-block').hide();
+// $('.socials').hide();
+
 $(document).ready(function(){
 
   $('.menu-icon').click(function(){
     $('.top__navegation--menu').toggleClass('active');
   });
-
-
-
 
 
 });  //end
